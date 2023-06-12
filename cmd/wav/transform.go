1da66b75fdf2f58f1142dcd9c5c734deeed2cb3e
lcrocket <liamcrocket@gmail.com>
2023-06-12 19:31:28 -0400

    Add concat

--- cmd/wav/transform.go
+++ cmd/wav/transform.go
@@ -119,3 +119,47 @@ func (w *Wav) ConvertToStereo() error {
 
 	return nil
 }
+
+// Concat takes another Wav struct and stitches the two audio files
+// together. The returned wav struct will have the number of channels
+// equal to the largest number of channels out of the two wavs being
+// concatenated.
+func (w *Wav) Concat(toAdd *Wav) error {
+	revertAddedWav := false
+
+	// TODO: Just like we're standardizing the number of channels,
+	// we need to think of the case where there are differing sample rates,
+	// and differing bit depths
+	if w.Channels == 2 || toAdd.Channels == 2 {
+		if w.Channels == 1 {
+			err := w.ConvertToStereo()
+			if err != nil {
+				return err
+			}
+		} else if toAdd.Channels == 1 {
+			err := toAdd.ConvertToStereo()
+			if err != nil {
+				return err
+			}
+
+			// So as to not cause any side effects to the added wav,
+			// set this flag to remember that the `toAdd` wav should
+			// be converted back to mono.
+			// Important to note that going from mono to stereo, then back
+			// to mono results in no data loss.
+			revertAddedWav = true
+		}
+	}
+
+	w.Data = append(w.Data, toAdd.Data...)
+	w.DataSize = w.DataSize + toAdd.DataSize
+
+	if revertAddedWav {
+		err := toAdd.ConvertToMono()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
