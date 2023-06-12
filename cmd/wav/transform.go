896a2f235b62d9e323f2ed2ad8f3b3397bf79de7
lcrocket <liamcrocket@gmail.com>
2023-06-12 19:30:10 -0400

    Add convert to stereo

--- cmd/wav/transform.go
+++ cmd/wav/transform.go
@@ -1,5 +1,11 @@
 package wav
 
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
 // SpeedUp speeds up the wav file by a specified factor.
 // This will not only increase the speed of the audio, but
 // will increase the pitch as well.
@@ -12,4 +18,26 @@ func (w *Wav) SpeedUp(factor float32) {
 // will lower the pitch as well.
 func (w *Wav) SlowDown(factor float32) {
 	w.SampleRate = uint32(float32(w.SampleRate) / factor)
+}
+
+// ConvertToStereo takes a mono wav struct and converts it to an
+// equivalent stereo wav struct
+func (w *Wav) ConvertToStereo() error {
+	if (w.Channels != uint16(1)) {
+		return fmt.Errorf("input must have 1 audio channel, but this one has %v", w.Channels)
+	}
+
+	w.Channels = 2
+	if w.DataSize > math.MaxUint32 / 2 {
+		return errors.New("file size too large to be converted to stereo")
+	}
+	w.DataSize *= 2
+	for i := 0; i < len(w.Data); i++ {
+		if len(w.Data[i].ChannelData) != 1 {
+			return errors.New("malformed wav struct")
+		}
+		w.Data[i].ChannelData = append(w.Data[i].ChannelData, w.Data[i].ChannelData[0])
+	}
+
+	return nil
 }
